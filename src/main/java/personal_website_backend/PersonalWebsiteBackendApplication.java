c0672641ea0abc22022f0be32bd7df5110e68e29
tdM05 <taddmao@gmail.com>
2024-10-29 17:36:22 -0400

    Initial commit

---
+++ src/main/java/personal_website_backend/PersonalWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package personal_website_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalWebsiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalWebsiteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/personal_website_backend/PersonalWebsiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package personal_website_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalWebsiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
