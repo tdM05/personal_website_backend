646fd5286be190c63e93b01b989b07d89352c1ad
tdM05 <taddmao@gmail.com>
2024-11-01 22:31:43 -0400

    added tdm05.github.io to cors so that it can access the api

--- src/main/java/personal_website_backend/WebConfig.java
+++ src/main/java/personal_website_backend/WebConfig.java
@@ -2,18 +2,30 @@ package personal_website_backend;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4024", "http://localhost:4025") // Allow requests from your frontend app
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4024"); // Local development
+        config.addAllowedOrigin("http://localhost:4025"); // Local development
+        config.addAllowedOrigin("http://localhost:5178"); // Local development
+        config.addAllowedOrigin("https://tdm05.github.io"); // Deployed website
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("OPTIONS");
+        source.registerCorsConfiguration("/api/**", config);
+        return new CorsFilter(source);
     }
 }
